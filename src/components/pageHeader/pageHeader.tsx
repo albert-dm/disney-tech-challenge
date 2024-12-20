1ab5fed0feebc0b0ddfac8821cabf987cc5a53ea
Albert Moreira <inbox@albert-dm.dev>
2024-12-20 18:34:00 -0300

    Improve search bar

--- src/components/pageHeader/pageHeader.tsx
+++ src/components/pageHeader/pageHeader.tsx
@@ -2,9 +2,11 @@ import LogoImage from "../../static/image/logo.png";
 import AvatarImage from "../../static/image/avatar.png";
 import { Link, useNavigate, useSearch } from "@tanstack/react-router";
 import { debounce } from "../../helpers/debounce";
+import { useEffect, useRef } from "react";
 
 export function PageHeader() {
   const navigate = useNavigate();
+  const searchInput = useRef<HTMLInputElement>(null);
   const searchText = debounce((text: string) => {
     if (text.length >= 3) {
     navigate({to: `/search?query=${text}`});
@@ -15,7 +17,13 @@ export function PageHeader() {
 
   const { query } = useSearch({
     strict: false,
-  })
+  });
+
+  useEffect(() => {
+    if (!query && searchInput.current) {
+      searchInput.current.value = "";
+    }
+  }, [query]);
 
   const handelSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const search = e.target.value;
@@ -26,7 +34,7 @@ export function PageHeader() {
       <Link to="/">
         <img src={LogoImage} alt="logo" />
       </Link>
-      <input type="text" placeholder="Find a character..." defaultValue={query} className="w-full bg-background py-2 px-4 rounded-3xl" onChange={handelSearch} />
+      <input ref={searchInput} type="text" placeholder="Find a character..." value={query} className="w-full bg-background py-2 px-4 rounded-3xl" onChange={handelSearch} />
       <Link to="/profile">
         <img src={AvatarImage} alt="Go to Profile" />
       </Link>
