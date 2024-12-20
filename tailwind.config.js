ba8819b360a6ba738bf07d3051d047fe0e028f56
Albert Moreira <inbox@albert-dm.dev>
2024-12-20 11:38:19 -0300

    Configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#f1f2f4",
+        primary: "#054553",
+      }
+    },
+  },
+  plugins: [],
+}
